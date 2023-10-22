960ec13a1797dfa662205d7aed644b3f4620b190
kfigaj <kfigaj87@gmail.com>
2023-10-22 12:49:27 +0200

    commit no4

--- 03-laczenie-z-baza-mysql.py
+++ 03-laczenie-z-baza-mysql.py
@@ -1,22 +1,28 @@
 import mysql.connector
 
 connection = mysql.connector.connect(
-    user='', password='', host='', database='python', auth_plugin='mysql_native_password')
+    user='karol', password='!Sqlkarol', host='127.0.0.1', database='python', auth_plugin='mysql_native_password')
 
 cursor = connection.cursor()
 
-insertQuery = "INSERT INTO users(username, city) VALUES(%(username)s, %(city)s)"
-insertData = {
-    'username': 'Marian',
-    'city': ' Krakow'
-}
-cursor.execute(insertQuery, insertData)
 
-connection.commit()
+checkQuery = "SELECT * FROM users WHERE username = %(username)s AND city = %(city)s"
+insertData = {
+    'username': 'Karol',
+    'city': 'Warszawa'
+}
+cursor.execute(checkQuery, insertData)
+existing_user = cursor.fetchone()
+
+if existing_user is None:
+    insertQuery = "INSERT INTO users(username, city) VALUES(%(username)s, %(city)s)"
+    cursor.execute(insertQuery, insertData)
+    connection.commit()
+    print("Użytkownik dodany pomyślnie.")
+else:
+    print("Podany użytkownik już istnieje.")
 
 query = 'SELECT id, username, city FROM users'
-
-
 cursor.execute(query)
 
 for (id, username, city) in cursor:
