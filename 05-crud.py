0de0c09ad9cc15c6fc70f2737725c1064d2c4097
kfigaj <kfigaj87@gmail.com>
2023-10-26 20:52:19 +0200

    commit no11

--- 05-crud.py
+++ 05-crud.py
@@ -1,15 +1,11 @@
 import mysql.connector
-from flask import Flask
-from flask import jsonify
-from flask import request
-from flask import Response
+from flask import Flask, jsonify, request, Response
 import jsonpickle
+from config import DATABASE_CONFIG
 
 
 def get_connection_to_database():
-    connection = mysql.connector.connect(
-        user='', password='', host='127.0.0.1', database='python1', auth_plugin='mysql_native_password')
-
+    connection = mysql.connector.connect(**DATABASE_CONFIG)
     return connection
 
 
@@ -26,31 +22,24 @@ app = Flask(__name__)
 @app.route('/users', methods=['GET'])
 def get_users():
     users = []
-    connection = get_connection_to_database()
-    cursor = connection.cursor(dictionary=True)
-    query = 'SELECT id, username, city FROM users'
-    cursor.execute(query)
-
-    for row in cursor:
-        users.append(User(row['id'], row['username'], row['city']))
-
-    connection.close()
-
+    with get_connection_to_database() as connection:
+        with connection.cursor(dictionary=True) as cursor:
+            query = 'SELECT id, username, city FROM users'
+            cursor.execute(query)
+            for row in cursor:
+                users.append(User(row['id'], row['username'], row['city']))
     response = Response(jsonpickle.encode(
         users, unpicklable=False), mimetype='application/json')
     response.headers['Title'] = 'CRUD_app'
-
     return response
 
 
 @app.route('/users', methods=['POST'])
 def add_user():
     request_data = request.get_json()
-
     try:
         connection = get_connection_to_database()
         cursor = connection.cursor()
-
         query = 'INSERT INTO users(username, city) VALUES(%(username)s, %(city)s)'
         cursor.execute(query, request_data)
         connection.commit()
@@ -58,19 +47,16 @@ def add_user():
         return jsonify(details=err.msg), 400
     finally:
         connection.close()
-
-    return request_data, 201
+    return jsonify(request_data), 201
 
 
 @app.route('/users/<user_id>', methods=['PUT'])
 def edit_user(user_id):
     request_data = request.get_json()
     request_data['user_id'] = user_id
-
     try:
         connection = get_connection_to_database()
         cursor = connection.cursor()
-
         query = 'UPDATE users SET username=%(username)s, city=%(city)s WHERE id=%(user_id)s'
         cursor.execute(query, request_data)
         connection.commit()
@@ -78,8 +64,7 @@ def edit_user(user_id):
         return jsonify(details=err.msg), 400
     finally:
         connection.close()
-
-    return request_data
+    return jsonify(request_data)
 
 
 @app.route('/users/<user_id>', methods=["DELETE"])
@@ -89,18 +74,15 @@ def delete_user(user_id):
     try:
         connection = get_connection_to_database()
         cursor = connection.cursor()
-
         query = 'DELETE from users WHERE id=%(user_id)s'
         cursor.execute(query, request_data)
         connection.commit()
-
         return jsonify(message="User deleted")
     except mysql.connector.Error as err:
         return jsonify(details=err.msg), 400
     finally:
         connection.close()
 
-    return jsonify()
 
-
-app.run()
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,7 @@
+DATABASE_CONFIG = {
+    'user': 'root',
+    'password': '!Sqlkarol',
+    'host': '127.0.0.1',
+    'database': 'python1',
+    'auth_plugin': 'mysql_native_password'
+}
